82d5cf902e40d18fa737331d693df4a9f20f25df
Elías <el.antican@duocuc.cl>
2024-11-05 23:00:24 -0300

    vistas

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/usuarios.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  userName: string | null = null;
+  userRole: string | null = null;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+    this.userRole = localStorage.getItem('userRole');
+    this.userName = localStorage.getItem('userName');
   }
 
+  capitalize(name: string | null): string | null {
+    if (!name) return null;
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  }
 }
